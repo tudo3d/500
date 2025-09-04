a40900b85186f202c6c0721dd98c71ee2030df91
biqu <tudo3dmichel@gmail.com>
2025-09-04 10:03:59 -0400

    generate-shaper-graph-x.sh modified - 04/09/2025 - 10:03:55

--- printer_data/config/scripts/generate-shaper-graph-x.sh
+++ printer_data/config/scripts/generate-shaper-graph-x.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-cat > /home/biqu/printer_data/config/scripts/generate-shaper-graph-y.sh <<'EOF'
-NEWY=$(ls -Art /tmp/resonances_y_*.csv | tail -n 1)
+cat > /home/biqu/printer_data/config/scripts/generate-shaper-graph-x.sh <<'EOF'
+NEWY=$(ls -Art /tmp/resonances_x_*.csv | tail -n 1)
 DATE=$(date +'%Y-%m-%d-%H%M%S')
 
 if [ -z "$NEWY" ]; then
-  echo "Nenhum arquivo /tmp/resonances_y_*.csv encontrado." >&2
+  echo "Nenhum arquivo /tmp/resonances_x_*.csv encontrado." >&2
   exit 1
 fi
 
@@ -16,6 +16,6 @@ fi
 /home/biqu/klipper/scripts/calibrate_shaper.py "$NEWY" -o /home/biqu/printer_data/config/input_shaper/resonances_x_"$DATE".png
 EOF
 
-chmod +x /home/biqu/printer_data/config/scripts/generate-shaper-graph-y.sh
+chmod +x /home/biqu/printer_data/config/scripts/generate-shaper-graph-x.sh
 
 
