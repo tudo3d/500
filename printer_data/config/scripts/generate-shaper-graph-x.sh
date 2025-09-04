63e2e760e1209665c4abd8dc9819f7ddf5bf31b1
biqu <tudo3dmichel@gmail.com>
2025-09-04 09:58:21 -0400

    generate-shaper-graph-x.sh modified - 04/09/2025 - 09:58:17

--- printer_data/config/scripts/generate-shaper-graph-x.sh
+++ printer_data/config/scripts/generate-shaper-graph-x.sh
@@ -1,5 +1,5 @@
-cat > /home/biqu/printer_data/config/scripts/generate-shaper-graph-y.sh <<'EOF'
 #!/bin/bash
+cat > /home/biqu/printer_data/config/scripts/generate-shaper-graph-y.sh <<'EOF'
 NEWY=$(ls -Art /tmp/resonances_y_*.csv | tail -n 1)
 DATE=$(date +'%Y-%m-%d-%H%M%S')
 
@@ -13,7 +13,9 @@ if [ ! -d "/home/biqu/printer_data/config/input_shaper" ]; then
     chown biqu:biqu /home/biqu/printer_data/config/input_shaper
 fi
 
-/home/biqu/klipper/scripts/calibrate_shaper.py "$NEWY" -o /home/biqu/printer_data/config/input_shaper/resonances_y_"$DATE".png
+/home/biqu/klipper/scripts/calibrate_shaper.py "$NEWY" -o /home/biqu/printer_data/config/input_shaper/resonances_x_"$DATE".png
 EOF
 
 chmod +x /home/biqu/printer_data/config/scripts/generate-shaper-graph-y.sh
+
+
