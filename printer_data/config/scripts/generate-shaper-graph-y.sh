ac96b4391d12ff4fa03357ccc44cac90247540b4
biqu <tudo3dmichel@gmail.com>
2025-09-04 10:35:51 -0400

    generate-shaper-graph-y.sh modified - 04/09/2025 - 10:35:47

--- printer_data/config/scripts/generate-shaper-graph-y.sh
+++ printer_data/config/scripts/generate-shaper-graph-y.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-NEWY=$(ls -Art /tmp/resonances_y_*.csv | tail -n 1)
-DATE=$(date +'%Y-%m-%d-%H%M%S')
 
-# se não encontrou nenhum CSV, sai com erro
-if [ -z "$NEWY" ]; then
-  echo "Nenhum arquivo /tmp/resonances_y_*.csv encontrado." >&2
-  exit 1
+SCRIPT_DIR=$( cd -- "$( dirname -- "$(realpath -- "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
+
+shellcheck source =. /configuration/scripts/common.sh
+source "$SCRIPT_DIR"/common.sh
+
+NEWX=$(find /tmp -name "resonances_y _*. csv" -printf '%T@ %p\n' 2> /dev/null | sort -n | tail -1 | cut -f2- -d" ")
+DATE=$(date +'%Y-%m-%d-%H%M%S' )
+outdir="${PRINTER_DATA_DIR}"/config/input_shaper
+if [ ! -d "${outdir}" ]
+then
+mkdir "${outdir}"
+chown "${USERNAME}:${USERGROUP]" "${outdir}"
 fi
 
-if [ ! -d "/home/biqu/printer_data/config/input_shaper" ]; then
-    mkdir -p /home/biqu/printer_data/config/input_shaper
-    chown biqu:biqu /home/biqu/printer_data/config/input_shaper
-fi
-
-/home/biqu/klipper/scripts/calibrate_shaper.py "$NEWY" -o /home/biqu/printer_data/config/input_shaper/resonances_y_"$DATE".png
+"${KLIPPER_DIR}"/scripts/calibrate_shaper.py "$NEWX" -o "${outdir}/resonances_y_${DATE}.png"
