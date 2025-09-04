94ed098e25ae78b6395cd021fe451c2023b315e9
biqu <tudo3dmichel@gmail.com>
2025-09-04 10:34:36 -0400

    generate-shaper-graph-x.sh modified - 04/09/2025 - 10:34:32

--- printer_data/config/scripts/generate-shaper-graph-x.sh
+++ printer_data/config/scripts/generate-shaper-graph-x.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-cat > /home/biqu/printer_data/config/scripts/generate-shaper-graph-x.sh <<'EOF'
-NEWY=$(ls -Art /tmp/resonances_x_*.csv | tail -n 1)
-DATE=$(date +'%Y-%m-%d-%H%M%S')
 
-if [ -z "$NEWY" ]; then
-  echo "Nenhum arquivo /tmp/resonances_x_*.csv encontrado." >&2
-  exit 1
+SCRIPT_DIR=$( cd -- "$( dirname -- "$(realpath -- "${BASH_SOURCE[0]}")" )" &> /dev/null && pwd )
+
+shellcheck source =. /configuration/scripts/common.sh
+source "$SCRIPT_DIR"/common.sh
+
+NEWX=$(find /tmp -name "resonances_x _*. csv" -printf '%T@ %p\n' 2> /dev/null | sort -n | tail -1 | cut -f2- -d" ")
+DATE=$(date +'%Y-%m-%d-%H%M%S' )
+outdir="${PRINTER_DATA_DIR}"/config/input_shaper
+if [ ! -d "${outdir}" ]
+then
+mkdir "${outdir}"
+chown "${USERNAME}:${USERGROUP]" "${outdir}"
 fi
 
-if [ ! -d "/home/biqu/printer_data/config/input_shaper" ]; then
-    mkdir -p /home/biqu/printer_data/config/input_shaper
-    chown biqu:biqu /home/biqu/printer_data/config/input_shaper
-fi
-
-/home/biqu/klipper/scripts/calibrate_shaper.py "$NEWY" -o /home/biqu/printer_data/config/input_shaper/resonances_x_"$DATE".png
-EOF
-
-chmod +x /home/biqu/printer_data/config/scripts/generate-shaper-graph-x.sh
-
-
+"${KLIPPER_DIR}"/scripts/calibrate_shaper.py "$NEWX" -o "${outdir}/resonances_x_${DATE}.png"
